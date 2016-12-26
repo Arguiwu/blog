b3c8ce0740edb61975224b43d8d8be3d88b5c7f4
vinson <wxgsorry@gmail.com>
2016-12-26 17:21:40 +0800

    cnodejs demo API edit https

--- app/actions/IndexActions.js
+++ app/actions/IndexActions.js
@@ -6,7 +6,7 @@ class IndexActions {
 		);
 	}
 	getDataList() {
-		fetch('http://cnodejs.org/api/v1/topics').then(response => response.json())
+		fetch('https://cnodejs.org/api/v1/topics').then(response => response.json())
 			.then(data => this.actions.setDataList(data))
 			.catch(e => console.log('报错啦', e))
 	}
